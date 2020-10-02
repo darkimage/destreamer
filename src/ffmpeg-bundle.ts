e786cac86464fbd8098f78cc432686d9dd743f20
Darkimage <luc-af@live.it>
2020-10-02 23:26:09 +0200

    Added comments

--- src/ffmpeg-bundle.ts
+++ src/ffmpeg-bundle.ts
@@ -1,9 +1,17 @@
 import path from 'path';
 import fs from 'fs';
 
+/**
+ * Contain the correct path of ffmpeg
+ */
 let ffmpegPath = require('ffmpeg-static');
 if (require('./isStandAlone').default) {
+	// If we are inside a standalone paackage (pkg npm) let's apply the patch for the virtual filesystem
+	// on windows
 	require('./standalone-patch');
+	// copy outside of the bundle the ffpmeg executable 
+	// otherwise child_process cannot access it
+	// maybe there is away to access it inside the pkg snapshop but i didn't find any for now.
 	fs.copyFileSync(ffmpegPath, path.join(path.dirname(process.execPath), path.basename(ffmpegPath)))
 	ffmpegPath = path.join(path.dirname(process.execPath), path.basename(ffmpegPath))
 }
--- src/isStandAlone.ts
+++ src/isStandAlone.ts
@@ -1,3 +1,6 @@
 const process = require('process');
 
+/**
+ * Are we being executed from a pkg??
+ */
 export default Boolean(process.pkg);
